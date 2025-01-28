ced0d51d5e3ffd7e15a73b947bf33ea390a5761b
lukasdanev <ludanev@gmail.com>
2025-01-28 23:11:02 +0100

    File from Sonnet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MathGame from './components/MathGame'
+
+function App() {
+  return (
+    <main className="container mx-auto p-4">
+      <MathGame />
+    </main>
+  )
+}
+
+export default App
